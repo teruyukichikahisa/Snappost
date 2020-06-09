596837bd8a7e7f207c2313da96d0a33111da4daa
teruyukichikahisa <teruyuki19851220@yahoo.co.jp>
2020-06-09 19:32:49 +0900

    change-function

--- db/migrate/20200605121651_add_name_to_user.rb
+++ db/migrate/20200605121651_add_name_to_user.rb
@@ -1,5 +1,12 @@
 class AddNameToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :name, :string, null: false, default: ""
+    add_column :users, :username, :string
+    add_column :users, :name, :string
+    add_column :users, :website, :string
+    add_column :users, :bio, :text
+    add_column :users, :phone, :integer
+    add_column :users, :gender, :string
   end
+  
 end
+
