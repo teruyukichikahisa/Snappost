1157c4622b3b03825aaea0f3d4a43ad3b99b4933
teruyukichikahisa <teruyuki19851220@yahoo.co.jp>
2020-06-09 12:16:16 +0900

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :website, :avatar, :bio, :email, :phone, :gender])
+        
     end
     
 end
