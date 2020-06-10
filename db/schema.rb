12f9826e5dba2282ab5780bba5072da4ff4e2754
teruyukichikahisa <teruyuki19851220@yahoo.co.jp>
2020-06-10 18:46:36 +0900

    delete

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,12 @@ ActiveRecord::Schema.define(version: 2020_06_05_154236) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", default: "", null: false
+    t.string "username"
+    t.string "name"
+    t.string "website"
+    t.text "bio"
+    t.integer "phone"
+    t.string "gender"
     t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
